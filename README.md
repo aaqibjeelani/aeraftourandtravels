ff3cdff7b7f5da06f4c87bfe3c7886a31e8109f8
Aaqib jeelani <ajvirus1@gmail.com>
2024-01-28 01:08:59 +0530

    mohsin

--- README.md
+++
@@ -1,2 +0,0 @@
-# aeraftourandtravels
- aeraftourandtravels
--- wp-content/themes/travel-boost/inc/admin/theme-notice.php
+++
@@ -1,8 +0,0 @@
-<div class="notice notice-info is-dismissible">
-    <h2><?php esc_html_e('Travel Boost Pro', 'travel-boost'); ?></h2>
-    <p><?php esc_html_e('Elevate your website with the Pro version, unlocking 30+ readymade templates, Gutenberg blocks, and patterns. Experience the full range of flexible features and dedicated support, empowering you to effortlessly take your website to new heights of excellence..', 'travel-boost'); ?></p>
-    <p>
-        <a class="button-primary" href="<?php echo esc_url('https://www.eaglevisionit.com/downloads/travel-boost/'); ?>" target="_blank"><?php esc_html_e('Get Travel Boost Pro', 'travel-boost'); ?></a>
-        <a class="button-secondary" href="<?php echo esc_url( admin_url( 'themes.php?page=travel-boost' ) ); ?>"><?php esc_html_e( 'Theme Info', 'travel-boost' ); ?></a>
-    </p>
-</div>
