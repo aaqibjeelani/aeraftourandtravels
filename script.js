b56cfb32072f024f07f1989b525144d0146d5c1e
Aaqib jeelani <ajvirus1@gmail.com>
2024-01-29 14:45:46 +0530

    updates

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Simulate a 5-second loading time (you can replace this with your actual loading logic)
+    setTimeout(function() {
+        document.getElementById("loader-container").style.display = "none";
+    }, 5000);
+});
